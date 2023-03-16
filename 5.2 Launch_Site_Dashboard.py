5085306b88b44c4b9db9a6f9a570b181e4de007c
Noissey24 <102809986+Noissey24@users.noreply.github.com>
2023-03-16 10:08:11 -0400

    Update 5.2 Launch_Site_Dashboard.py

--- 5.2 Launch_Site_Dashboard.py
+++ 5.2 Launch_Site_Dashboard.py
@@ -7,7 +7,7 @@ from dash.dependencies import Input, Output
 import plotly.express as px
 
 # Read the airline data into pandas dataframe
-spacex_df = pd.read_csv(r"C:\Users\Neil\Documents\Neil\Escritorio\Curso Python\OwnProjects\spacex_launch_dash.csv")
+spacex_df = pd.read_csv(r"spacex_launch_dash.csv")
 max_payload = spacex_df['Payload Mass (kg)'].max()
 min_payload = spacex_df['Payload Mass (kg)'].min()
 
